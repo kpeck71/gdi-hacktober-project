903a19e1c178e87f4604e78cd10074664e1ede30
MFeuer23 <mallory.feuer@gmail.com>
2018-10-20 16:15:12 -0400

    edit gameplay

--- gameplay.js
+++ gameplay.js
@@ -1,4 +1,4 @@
 function playGame() {
-  (Math.floor(Math.random() * 2) == 0) ? document.getElementByClass("cat-score").value += 1 : document.getElementByClass("dog-score").value += 1;
+  (Math.floor(Math.random() * 2) == 0) ? parseInt(document.getElementsByClassName("cat-score")[0].innerHTML) += 1 : parseInt(document.getElementsByClassName("dog-score")[0].innerHTML) += 1;
 
 }
