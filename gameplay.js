aeacfaf37e5053915785c416cea92d2ed71d55b5
Amanda <amandarebecca89@gmail.com>
2018-10-20 16:16:01 -0400

    Update gameplay.js

--- gameplay.js
+++ gameplay.js
@@ -1,4 +1,3 @@
 function playGame() {
   (Math.floor(Math.random() * 2) == 0) ? parseInt(document.getElementsByClassName("cat-score")[0].innerHTML) += 1 : parseInt(document.getElementsByClassName("dog-score")[0].innerHTML) += 1;
-
 }
